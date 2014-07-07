f2e7a956628c560eee35da96f5ccb8844e49ac01
Yu Feng <yfeng1@h2ologin2.ncsa.illinois.edu>
2014-07-07 16:09:01 -0500

    update repartition tool add -v.

--- utils/bigfile-copy.c
+++ utils/bigfile-copy.c
@@ -15,13 +15,20 @@ int main(int argc, char * argv[]) {
     BigFile bf = {0};
     BigBlock bb = {0};
     BigBlock bbnew = {0};
-
+    int verbose = 0;
     int ch;
     int Nfile = -1;
-    while(-1 != (ch = getopt(argc, argv, "n:"))) {
+    size_t buffersize = 256 * 1024 * 1024;
+    while(-1 != (ch = getopt(argc, argv, "n:vb:"))) {
         switch(ch) {
             case 'n':
                 Nfile = atoi(optarg);
+                break;
+            case 'b':
+                sscanf(optarg, "%td", &buffersize);
+                break;
+            case 'v':
+                verbose = 1;
                 break;
             default:
                 usage();
@@ -39,10 +46,11 @@ int main(int argc, char * argv[]) {
         fprintf(stderr, "failed to open: %s\n", big_file_get_error_message());
         exit(1);
     }
-    if(Nfile == -1) {
+    if(Nfile == -1 || bb.Nfile == 0) {
         Nfile = bb.Nfile;
     }
-    size_t newsize[Nfile];
+    /* avoid Nfile == 0 */
+    size_t newsize[Nfile + 10];
     int i;
     for(i = 0; i < Nfile; i ++) {
         newsize[i] = (i + 1) * bb.size / Nfile
@@ -66,30 +74,34 @@ int main(int argc, char * argv[]) {
         big_block_set_attr(&bbnew, attr->name, attr->data, attr->dtype, attr->nmemb);
     }
     
-    /* copy data */
-    size_t buffersize = 256 * 1024 * 1024;
-    size_t chunksize = buffersize / (bb.nmemb * dtype_itemsize(bb.dtype));
-    BigBlockPtr ptrnew;
-    ptrdiff_t offset;
-    BigArray array;
+    if(bb.nmemb > 0 && bb.size > 0) {
+        /* copy data */
+        size_t chunksize = buffersize / (bb.nmemb * dtype_itemsize(bb.dtype));
+        BigBlockPtr ptrnew;
+        ptrdiff_t offset;
+        BigArray array;
 
-    for(offset = 0; offset < bb.size; ) {
-        if(0 != big_block_read_simple(&bb, offset, chunksize, &array, NULL)) {
-            fprintf(stderr, "failed to read original: %s\n", big_file_get_error_message());
-            exit(1);
-        }
-        if(0 != big_block_seek(&bbnew, &ptrnew, offset)) {
-            fprintf(stderr, "failed to seek new: %s\n", big_file_get_error_message());
-            exit(1);
-        }
+        for(offset = 0; offset < bb.size; ) {
+            if(0 != big_block_read_simple(&bb, offset, chunksize, &array, NULL)) {
+                fprintf(stderr, "failed to read original: %s\n", big_file_get_error_message());
+                exit(1);
+            }
+            if(0 != big_block_seek(&bbnew, &ptrnew, offset)) {
+                fprintf(stderr, "failed to seek new: %s\n", big_file_get_error_message());
+                exit(1);
+            }
 
-        if(0 != big_block_write(&bbnew, &ptrnew, &array)) {
-            fprintf(stderr, "failed to write new: %s\n", big_file_get_error_message());
-            exit(1);
-        }
+            if(0 != big_block_write(&bbnew, &ptrnew, &array)) {
+                fprintf(stderr, "failed to write new: %s\n", big_file_get_error_message());
+                exit(1);
+            }
 
-        free(array.data);
-        offset += array.dims[0];
+            free(array.data);
+            offset += array.dims[0];
+            if(verbose) {
+                fprintf(stderr, "%td / %td done\r", offset, bb.size);
+            }
+        }
     }
     if(0 != big_block_close(&bbnew)) {
         fprintf(stderr, "failed to close new: %s\n", big_file_get_error_message());
