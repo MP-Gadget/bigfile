df26a93838e5ed439b739f823431cf011cbbde1e
Yu Feng <yfeng1@coma.hpc1.cs.cmu.edu>
2015-02-03 02:32:39 -0500

    fixes bugs in mpi

--- python/__init__.py
+++ python/__init__.py
@@ -1,4 +1,13 @@
 from pyxbigfile import BigBlock
-from pyxbigfile import BigFile
+from pyxbigfile import BigFile as BigFileBase
 from pyxbigfile import set_buffer_size
-import bigfilempi as mpi
+import bigfilempi
+
+class BigFile(BigFileBase):
+    def mpi_create(self, comm, block, dtype=None, size=None,
+            Nfile=1):
+        return bigfilempi.create_block(comm, self, block, dtype, size, Nfile)
+
+    def mpi_create_from_data(self, comm, block, localdata, Nfile=1):
+        bigfilempi.write_block(comm, self, block, localdata, Nfile)
+
