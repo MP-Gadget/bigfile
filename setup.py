6b1938464936c117773ea0476b6beb0a1bd0d3bc
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-02-15 20:06:31 -0800

    0.1.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ extensions = [
             include_dirs = ["src/", numpy.get_include()])]
 
 setup(
-    name="bigfile", version="0.1.5",
+    name="bigfile", version="0.1.6",
     author="Yu Feng",
     author_email="rainwoodman@gmail.com",
     url="http://github.com/rainwoodman/bigfile",
