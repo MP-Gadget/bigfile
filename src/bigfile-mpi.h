dbbac7e019fd1a2b4691dda3c28cdf14e1665737
Simeon Bird <sbird4@jhu.edu>
2016-03-07 19:57:54 -0500

    Write some documentation for the C api

--- src/bigfile-mpi.h
+++ src/bigfile-mpi.h
@@ -3,13 +3,59 @@
 #include <mpi.h>
 #include "bigfile.h"
 
+/** Open a Bigfile: this stats the directory tree that constitutes the BigFile format.
+ * It initialises the BigFile structure.
+ * Arguments:
+ * @param BigFile bf - pointer to uninitialised structure.
+ * @param const char * basename - String containing directory to put snapshot in.
+ * @param MPI_Comm comm - MPI communicator. Does nothing except make sure all tasks exit this function together.
+ * @returns 0 if successful. */
 int big_file_mpi_open(BigFile * bf, const char * basename, MPI_Comm comm);
+
+/** Create a Bigfile: this creates the directory tree that constitutes the BigFile format.
+ * It initialises the BigFile structure.
+ * Arguments:
+ * @param BigFile bf - pointer to uninitialised structure.
+ * @param const char * basename - String containing directory to put snapshot in.
+ * @param MPI_Comm comm - MPI communicator. Does nothing except make sure all tasks exit this function together.
+ * @returns 0 if successful. */
 int big_file_mpi_create(BigFile * bf, const char * basename, MPI_Comm comm);
+
+/** Open a BigBlock:
+ * A BigBlock stores a two dimesional table of nmemb columns and size rows. Numerical typed columns are supported.
+ * Arguments:
+ * @param BigFile bf - pointer to opened BigFile structure.
+ * @param BigBlock block - pointer to initialised BigBlock to open.
+ * @param const char * basename - Name of the block to open at. eg. "header". Must already exist.
+ * @param MPI_Comm comm - MPI communicator to use.
+ * @returns 0 if successful, -1 if could not open block. */
 int big_file_mpi_open_block(BigFile * bf, BigBlock * block, const char * blockname, MPI_Comm comm);
+
+/** Create a BigBlock:
+ * A BigBlock stores a two dimesional table of nmemb columns and size rows. Numerical typed columns are supported.
+ * Arguments:
+ * @param BigFile bf - pointer to opened BigFile structure.
+ * @param BigBlock block - pointer to uninitialised BigBlock.
+ * @param const char * basename - Name of the block to initialise at. eg. "header": what happens if this structure exists already?
+ * @param dtype - string denoting the type of the block. This has a normalised form, like "i8" for a 64-bit integer.
+ * See documentation for dtype_parse. Can be NULL if only attributes will be stored.
+ * @param - nmemb - Number of columns that will be stored in this block. Can be zero.
+ * @param Nfile - Number of files to use for this block on disc. This is an implementation detail;
+ * you will never need it to read the BigFile.
+ * @param - size Number of rows of type dtype that will be stored in this block. Can be zero.
+ * @param MPI_Comm comm - MPI communicator to use.
+ * @returns 0 if successful. */
 int big_file_mpi_create_block(BigFile * bf, BigBlock * block, const char * blockname, const char * dtype, int nmemb, int Nfile, size_t size, MPI_Comm comm);
 
+/** Close the BigFile, and free memory associated with it. Once closed, it should not be re-used.*/
 int big_file_mpi_close(BigFile * bf, MPI_Comm comm);
+
+/**Helper function for big_file_mpi_create_block, above*/
 int big_block_mpi_create(BigBlock * bb, const char * basename, const char * dtype, int nmemb, int Nfile, size_t fsize[], MPI_Comm comm);
+
+/** Close the BigBlock, and free memory associated with it. Once closed, it should not be re-used.*/
 int big_block_mpi_close(BigBlock * block, MPI_Comm comm);
+
+/** Helper function for big_file_mpi_open_block, above*/
 int big_block_mpi_open(BigBlock * bb, const char * basename, MPI_Comm comm);
 #endif
--- src/bigfile.h
+++ src/bigfile.h
@@ -25,7 +25,7 @@ typedef struct BigBlockAttrSet {
 } BigBlockAttrSet;
 
 typedef struct BigBlock {
-    char dtype[8]; /* numpy style 
+    char dtype[8]; /* numpy style
                         dtype[0] <: little endian, > big endian, = machine endian
                         dtype[1] type char
                         dtype[2:] width in bytes
@@ -44,7 +44,7 @@ typedef struct BigBlock {
 typedef struct BigBlockPtr{
     int fileid;
     ptrdiff_t roffset; /* offset within a file */
-    ptrdiff_t aoffset; /* abs offset */ 
+    ptrdiff_t aoffset; /* abs offset */
 } BigBlockPtr;
 
 /**
@@ -87,7 +87,17 @@ char * big_file_get_error_message(void);
 void big_file_set_error_message(char * msg);
 void big_file_checksum(unsigned int * sum, void * buf, size_t size);
 
+/** Open a Bigfile: this stats the directory tree, but does not open the file.
+ * It initialises the BigFile structure.
+ * Arguments:
+ * @param BigFile bf - pointer to uninitialised structure.
+ * @param const char * basename - String containing directory to put snapshot in.*/
 int big_file_open(BigFile * bf, const char * basename);
+
+/** Create a Bigfile: this makes the directory tree and initialises the BigFile structure.
+ * Arguments:
+ * @param BigFile bf - pointer to uninitialised structure.
+ * @param const char * basename - String containing directory to put snapshot in.*/
 int big_file_create(BigFile * bf, const char * basename);
 int big_file_list(BigFile * bf, char *** blocknames, int * Nblocks);
 int big_file_open_block(BigFile * bf, BigBlock * block, const char * blockname);
@@ -100,15 +110,48 @@ int big_block_open(BigBlock * bb, const char * basename);
 int big_block_clear_checksum(BigBlock * bb);
 int big_block_create(BigBlock * bb, const char * basename, const char * dtype, int nmemb, int Nfile, const size_t fsize[]);
 int big_block_close(BigBlock * block);
+
+/** Initialise BigBlockPtr to the place in the BigBlock offset elements from the beginning of the block.
+ * This allows you to write into the BigBlock at a position other than the beginning.
+ * @param offset - Position to seek to in units of the size of the array element, eg, 8 bytes for an i8. If offset == -1, seek to the end of the array.
+ * All other offset < 0 seek array_size + 1 - offset.*/
 int big_block_seek(BigBlock * bb, BigBlockPtr * ptr, ptrdiff_t offset);
 int big_block_seek_rel(BigBlock * bb, BigBlockPtr * ptr, ptrdiff_t rel);
 int big_block_eof(BigBlock * bb, BigBlockPtr * ptr);
 int big_block_read(BigBlock * bb, BigBlockPtr * ptr, BigArray * array);
 int big_block_read_simple(BigBlock * bb, ptrdiff_t start, ptrdiff_t size, BigArray * array, const char * dtype);
 
+/** Write data stored in a BigArray to a BigBlock.
+ * You cannot write beyond the end of the size of the block.
+ * The value may be a (small) array.
+ * Arguments:
+ * @param block - pointer to opened BigBlock
+ * @param ptr - Absolute position to write to in the file. Construct this with a call to big_block_seek.
+ * @param array - BigArray containing the data which should be written.
+ * @returns 0 if successful. */
 int big_block_write(BigBlock * bb, BigBlockPtr * ptr, BigArray * array);
 int big_block_remove_attr(BigBlock * block, const char * attrname);
+
+/** Set an attribute on a BigBlock: attributes are plaintext key-value pairs stored in a special file in the Block directory.
+ * The value may be a (small) array.
+ * Arguments:
+ * @param block - pointer to opened BigBlock
+ * @param attrname - name of the attribute to store.
+ * @param data - pointer to data to store
+ * @param dtype - Type of data array in the format used by dtype.
+ * @param nmemb - Number of members in the data array.
+ * @returns 0 if successful. */
 int big_block_set_attr(BigBlock * block, const char * attrname, const void * data, const char * dtype, int nmemb);
+
+/** Get an attribute on a BigBlock: attributes are plaintext key-value pairs stored in a special file in the Block directory.
+ * Attribute value is stored in the memory pointed to by data, so make sure it is big enough!
+ * Arguments:
+ * @param block - pointer to opened BigBlock
+ * @param attrname - name of the attribute to store.
+ * @param data - pointer to data in which to place attribute
+ * @param dtype - Type of data array in the format used by dtype.
+ * @param nmemb - Number of members to get. Must be equal to number of members
+ * originally stored or an error is raised and -1 returned.*/
 int big_block_get_attr(BigBlock * block, const char * attrname, void * data, const char * dtype, int nmemb);
 BigBlockAttr * big_block_lookup_attr(BigBlock * block, const char * attrname);
 BigBlockAttr * big_block_list_attrs(BigBlock * block, size_t * count);
@@ -123,6 +166,32 @@ char dtype_kind(const char * dtype);
 int dtype_needswap(const char * dtype);
 int dtype_itemsize(const char * dtype);
 
+/** Create a BigArray from raw memory.
+ * A BigArray is a checked, multidimensional, array format. It is used to provide metadata
+ * about a raw void pointer to big_block_write. It is an implementation detail of this library, not stored on disc.
+ * This routine constructs the BigArray.
+ * Data is not copied, so the memory must not be freed before the BigArray is deallocated.
+ * @params array - uninitialised BigArray.
+ * @params buf - pointer to raw memory to be stored in the BigArray.
+ * @params dtype - type of the data array as a string.
+ * @params ndim - Number of dimensions to use in the BigArray.
+ * Note that a BigArray can have up to 32 dimensions, but only 2 of them can be stored in a BigFile.
+ * @params dims - list of integers containing the size of each dimension of the BigArray
+ * @params strides - Integers containing the size of each element of the BigArray, for use with BigArrayIter.
+ * This is a BigArray implementation of numpy strides, so see documentation here: http://www.scipy-lectures.org/advanced/advanced_numpy/#indexing-scheme-strides
+ * For a multidimensional array with C ordering and N dimensions,
+ * strides[i] is the length of the array in the ith dimension.
+ * strides[N-1] is the size of a single element.
+ * For BigArrays to be used in a BigFile,
+ * strides[1] should be the size of one element, dtype_itemsize(dtype).
+ * strides[0] should be one row: dims[1] * strides[1].
+ * The array can be ordered in other ways. For example, if you have:
+ *
+ * struct P {  double pos[3];  double vel[3]; };
+ *
+ * You can use strides[1] = sizeof(double), strides[0] = sizeof(struct P).
+ * Then set buf to &P[0].pos[0] to dump position or &P[0].vel[0] to dump velocity.
+ */
 int big_array_init(BigArray * array, void * buf, const char * dtype, int ndim, const size_t dims[], const ptrdiff_t strides[]);
 int big_array_iter_init(BigArrayIter * iter, BigArray * array);
 void big_array_iter_advance(BigArrayIter * iter);
