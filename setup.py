770bf9c1ad907008aa11c131e687ddc31862f5d9
Yu Feng <rainwoodman@gmail.com>
2018-05-29 19:02:39 -0700

    add more features to bigfile-convert regarding fits.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     zip_safe = False,
     package_dir = {'bigfile': 'bigfile'},
     install_requires=['cython', 'numpy'],
-    scripts = ['scripts/hdf2bigfile'],
+    scripts = ['scripts/bigfile-convert'],
     packages= ['bigfile', 'bigfile.tests'],
     license='BSD-2-Clause',
     ext_modules = cythonize(extensions)
