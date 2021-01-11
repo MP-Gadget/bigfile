5befa36835d8be7c361576eeba11980a5e207bf9
Yu Feng <rainwoodman@gmail.com>
2021-01-10 23:36:05 -0800

    fix check_tag.sh

--- check_tag.sh
+++ check_tag.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [[ -n "$1" ]]; then
-    if ! grep "$1" "$1"; then
+    if ! grep "$1" "$2"; then
         echo Tag $1 does not match setup.py version. Bail.
         exit 1
     fi
