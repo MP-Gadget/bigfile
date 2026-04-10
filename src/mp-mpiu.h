05439cdc473f99854c7db03bdbc42bec2a34885f
Simeon Bird <sbird@ucr.edu>
2026-04-10 15:26:19 -0700

    Remove code in mp-mpiu we were not using

--- src/mp-mpiu.h
+++ src/mp-mpiu.h
@@ -1,58 +1,6 @@
 #ifndef _MPIU_H_
 #define _MPIU_H_
 
-typedef void * (*mpiu_malloc_func)(const char * name, size_t size, const char * file, const int line, void * userdata);
-typedef void (*mpiu_free_func)(void * ptr, const char * file, const int line, void * userdata);
-
-void * mpiu_malloc(const char * name, size_t size, const char * file, const int line);
-void mpiu_free(void * ptr, const char * file, const int line);
-
-void mpiu_set_malloc(mpiu_malloc_func malloc, mpiu_free_func free, void * userdata);
-void MPIU_Set_verbose_malloc(MPI_Comm comm);
-
-/*
- * Set the MPIU memory allocator. MPIU uses the allocator to provided a hook for tracking allocations of significance.
- * */
-#define MPIU_SetMalloc mpiu_set_malloc
-#define MPIU_MallocT(name, type, nmemb) MPIU_Malloc(name, sizeof(type), nmemb, __FILE__, __LINE__)
-#define MPIU_Malloc(name, elsize, nmemb) mpiu_malloc(name, ((size_t)(elsize)) * nmemb, __FILE__, __LINE__)
-#define MPIU_Free(ptr) mpiu_free(ptr, __FILE__, __LINE__)
-
-/*
- * MPIU_Alltoallv:
- * a Alltoallv can automatically switch to a sparse implementation
- */
-enum MPIU_AlltoallvSparsePolicy {
-    AUTO = 0,
-    DISABLED = 1,
-    REQUIRED = 2
-};
-
-int MPIU_Alltoallv(void *sendbuf, int *sendcnts, int *sdispls,
-        MPI_Datatype sendtype, void *recvbuf, int *recvcnts,
-        int *rdispls, MPI_Datatype recvtype, MPI_Comm comm,
-        enum MPIU_AlltoallvSparsePolicy policy);
-
-/*
- * Returns the rank that contains the first result matching the MPI_Op.
- * op can be MPI_MIN or MPI_MAX. This function works around potentially buggy
- * MPI_MINLOC and MPI_MAXLOC implemenations.
- * */
-int
-MPIU_GetLoc(const void * base, MPI_Datatype type, MPI_Op op, MPI_Comm comm);
-
-/*
- * Gathers from all ranks to root. if recvbuffer is NULL, allocate new memory with MPIU_Malloc.
- */
-void *
-MPIU_Gather (MPI_Comm comm, int root, const void * sendbuffer, void * recvbuffer, int nsend, size_t elsize, int * totalnrecv);
-
-/*
- * Scatter from root to all ranks. if recvbuffer is NULL, allocate new memory with MPIU_Malloc.
- */
-void *
-MPIU_Scatter (MPI_Comm comm, int root, const void * sendbuffer, void * recvbuffer, int nrecv, size_t elsize, int * totalnsend);
-
 /* Segment a MPI Comm into 'groups', such that distributed data in each group is roughly even.
  * NOTE: this API needs some revision to incorporate some of the downstream behaviors. Currently
  * the internal data structure is directly accessed by downstream.
@@ -64,7 +12,6 @@ typedef struct MPIU_Segmenter {
     int GroupID; /* ID of the group of this rank */
     int ThisSegment; /* SegmentID of the local data chunk on this rank*/
 
-    size_t totalsize;
     int segment_start; /* segments responsible in this group */
     int segment_end;
 
